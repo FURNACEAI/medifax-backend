4d96843b50eab8cc9ae1521c86179f8756d720fe
Bryan Richard <bryan@furnaceai.com>
2018-02-22 16:08:47 -0700

    Fixed bug with auth()
    auth() function failed when the email/username didn't exist in the database.

--- medifax-employees/employee.py
+++ medifax-employees/employee.py
@@ -55,6 +55,12 @@ def list(event, context):
     return response
 
 def auth(event, context):
+    """
+    Attempts to verify a user's login credentials
+    Fetches the hashed version of the password via the username and attempts to verify it with pbkdf2_sha256.verify()
+
+    Returns a JSON response with either "Success" or "Error".
+    """
     data = json.loads(event['body'])
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
@@ -63,13 +69,20 @@ def auth(event, context):
         FilterExpression=Attr("email").eq(data['email'])
     )
 
-    if pbkdf2_sha256.verify(data['password'], result['Items'][0]['password']):
-        print ('Password Correct')
-    else:
-        print ('Password incorrect')
+    ## Set the default response to failure and then test for success.
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({"message": "Error", "messageDesc": "Username or password is incorrect."})
+    }
 
+    if len(result['Items']) > 0: # Sanity check to see if the username was correct
+        if pbkdf2_sha256.verify(data['password'], result['Items'][0]['password']):
+            response = {
+                "statusCode": 200,
+                "body": json.dumps({"message": "Success", "messageDesc": "Log successful."})
+            }
 
-    # return response
+    return response
 
 
 if __name__ == '__main__':
@@ -82,8 +95,8 @@ if __name__ == '__main__':
         },
         "body": json.dumps({
 		"email": "www555@domain.com",
-        "password": "some_guid"
+        "password": "XXXsome_guid"
 	})
     }
     test_json = json.loads(json.dumps(test_data))
-    auth(test_json, '')
+    print(auth(test_json, ''))
