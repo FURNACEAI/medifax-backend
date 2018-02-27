ed7622a41d4c078a3077dee7d923f63c450f5444
Bryan Richard <bryan@furnaceai.com>
2018-02-27 11:34:33 -0700

    Cloned the employee functions to a customer ver

---
+++ medifax-customers/employees/delete.py
@@ -0,0 +1,23 @@
+import json
+import os
+import boto3
+from boto3.dynamodb.conditions import Key
+
+def delete(event, context):
+    """
+    Deletes an Employee from the database
+    """
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    table.delete_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({"message": "Success"})
+    }
+
+    return response
