4a34c08b448fcd4d068dfc86cb85f8a9fbe07d57
Bryan Richard <bryan@furnaceai.com>
2018-04-04 17:07:25 -0600

    Fleshing out the remaining data fields for a customer.

--- medifax-customers/customers/create.py
+++ medifax-customers/customers/create.py
@@ -66,7 +66,14 @@ def create(event, context):
     cleartext_pwd = generate_pw(8)
     usrpwd = pbkdf2_sha256.encrypt(cleartext_pwd, rounds=200000, salt_size=16)
     uid = str(uuid.uuid1())
+
+    # Creation and modify timestamp
     timestamp = int(time.time())
+
+    # Replace any empty values with Python 'None' type
+    for key, value in data.items():
+        if not value:
+            data[key] = None
 
     item = {
         'id': uid,
@@ -85,6 +92,82 @@ def create(event, context):
         'password': usrpwd,
         'active': 'true',
         'status': 'Awaiting HIPAA Consent',
+        'blood_pressure_systolic': None,
+        'blood_pressure_diastolic': None,
+        'height': None,
+        'weight': None,
+        'heart_rate': None,
+        'bmi': None,
+        'gender': None,
+        'current_problems_0': None,
+        'current_problems_1': None,
+        'current_problems_2': None,
+        'current_problems_3': None,
+        'current_problems_4': None,
+        'current_problems_5': None,
+        'current_problems_6': None,
+        'current_problems_7': None,
+        'current_problems_8': None,
+        'current_problems_9': None,
+        'medication_name_0': None,
+        'medication_name_1': None,
+        'medication_name_2': None,
+        'medication_name_3': None,
+        'medication_name_4': None,
+        'medication_name_5': None,
+        'medication_name_6': None,
+        'medication_name_7': None,
+        'medication_name_8': None,
+        'medication_name_9': None,
+        'medication_dose_0': None,
+        'medication_dose_1': None,
+        'medication_dose_2': None,
+        'medication_dose_3': None,
+        'medication_dose_4': None,
+        'medication_dose_5': None,
+        'medication_dose_6': None,
+        'medication_dose_7': None,
+        'medication_dose_8': None,
+        'medication_dose_9': None,
+        'medication_freq_0': None,
+        'medication_freq_1': None,
+        'medication_freq_2': None,
+        'medication_freq_3': None,
+        'medication_freq_4': None,
+        'medication_freq_5': None,
+        'medication_freq_6': None,
+        'medication_freq_7': None,
+        'medication_freq_8': None,
+        'medication_freq_9': None,
+        'patient_preferences': None,
+        'patient_consents': None,
+        'family_history': None,
+        'allergies': None,
+        'referrals': None,
+        'lab_results': None,
+        'care_plan': None,
+        'lifestyle_history': None,
+        'social_history': None,
+        'dentist_name': None,
+        'dentist_phone': None,
+        'dentist_email': None,
+        'dental_condition': None,
+        'ins_planid_dental': None,
+        'ins_provider_dental': None,
+        'ins_street_addr_dental': None,
+        'ins_city_dental': None,
+        'ins_state_dental': None,
+        'ins_zipcode_dental': None,
+        'ins_phone_dental': None,
+        'ins_email_dental': None,
+        'ins_planid_med': None,
+        'ins_provider_med': None,
+        'ins_street_addr_med': None,
+        'ins_city_med': None,
+        'ins_state_med': None,
+        'ins_zipcode_med': None,
+        'ins_phone_med': None,
+        'ins_email_med': None,
         'created_on': timestamp,
         'updated_on': timestamp,
     }
@@ -103,9 +186,10 @@ if __name__ == '__main__':
     os.environ["DYNAMODB_TABLE"] = 'medifax-backend-customers-dev'
     payload = {
         "body": json.dumps({
-            "first_name": 'form.first_name.data',
-            "last_name": 'form.last_name.data',
-            "email": 'xxxform.email.data',
+            "first_name": 'James',
+            "middle_initial": 'T',
+            "last_name": 'Matt',
+            "email": 'testing@example.com',
             "home_phone": None,
             "mobile_phone": '8019539821',
             "street_address": '3356 S 1100 E',
@@ -113,7 +197,8 @@ if __name__ == '__main__':
             "city": 'Los Angeles',
             "state": 'CA',
             "zipcode": '90245',
-            "dob": '10/10/1910'
+            "dob": '10/10/1910',
+            "active": 'true'
         })
     }
 
--- medifax-customers/customers/update.py
+++ medifax-customers/customers/update.py
@@ -9,39 +9,182 @@ def update(event, context):
     data = json.loads(event['body'])
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-
-
+    # Update time
+    timestamp = int(time.time() * 1000)
     # Check if a password was passed to the event param. If not, use the existing password.
     if not data['password']:
-        result = table.scan(
-            FilterExpression=Attr("email").eq(data['email'])
+        result = table.get_item(
+            Key={
+                'id': event['pathParameters']['id']
+            }
         )
-        data['password'] = result['Items'][0]['password']
-
+        data['password'] = result['Item']['password']
     else:
         usrpwd = pbkdf2_sha256.encrypt(data['password'], rounds=200000, salt_size=16)
         data['password'] = usrpwd
 
-    timestamp = int(time.time() * 1000)
+    # Critical for DynamoDB: Replace any empty values with Python 'None' type
+    for key, value in data.items():
+        if not value:
+            data[key] = None
 
     attr_names = {
-        '#user_role': "user_role",
         '#name': "name",
         '#first': "first",
+        '#middle_initial': "middle_initial",
+        '#home_address': "home_address",
+        '#phone': "phone",
+        '#home': "home",
+        '#mobile': "mobile",
+        '#street_address': "street_address",
+        '#street_address_2': "street_address_2",
+        '#city': "city",
+        '#state': "state",
+        '#zipcode': "zipcode",
         '#last': "last",
-        '#active': "active",
         '#email': 'email',
         '#password': 'password',
+        '#dob': 'dob',
+        '#height': 'height',
+        '#weight': 'weight',
+        '#heart_rate': 'heart_rate',
+        '#active': 'active',
+        '#status': 'status',
+        '#gender': 'gender',
+        '#patient_preferences': 'patient_preferences',
+        '#patient_consents': 'patient_consents',
+        '#family_history': 'family_history',
+        '#allergies': 'allergies',
+        '#referrals': 'referrals',
+        '#lab_results': 'lab_results',
+        '#care_plan': 'care_plan',
+        '#lifestyle_history': 'lifestyle_history',
+        '#social_history': 'social_history',
+        '#bmi': 'bmi',
+        '#blood_pressure_systolic': 'blood_pressure_systolic',
+        '#blood_pressure_diastolic': 'blood_pressure_diastolic',
+        '#current_problems_0': 'current_problems_0',
+        '#current_problems_1': 'current_problems_1',
+        '#current_problems_2': 'current_problems_2',
+        '#current_problems_3': 'current_problems_3',
+        '#current_problems_4': 'current_problems_4',
+        '#current_problems_5': 'current_problems_5',
+        '#current_problems_6': 'current_problems_6',
+        '#current_problems_7': 'current_problems_7',
+        '#current_problems_8': 'current_problems_8',
+        '#current_problems_9': 'current_problems_9',
+        '#medication_name_0': 'medication_name_0',
+        '#medication_name_1': 'medication_name_1',
+        '#medication_name_2': 'medication_name_2',
+        '#medication_name_3': 'medication_name_3',
+        '#medication_name_4': 'medication_name_4',
+        '#medication_name_5': 'medication_name_5',
+        '#medication_name_6': 'medication_name_6',
+        '#medication_name_7': 'medication_name_7',
+        '#medication_name_8': 'medication_name_8',
+        '#medication_name_9': 'medication_name_9',
+        '#medication_dose_0': 'medication_dose_0',
+        '#medication_dose_1': 'medication_dose_1',
+        '#medication_dose_2': 'medication_dose_2',
+        '#medication_dose_3': 'medication_dose_3',
+        '#medication_dose_4': 'medication_dose_4',
+        '#medication_dose_5': 'medication_dose_5',
+        '#medication_dose_6': 'medication_dose_6',
+        '#medication_dose_7': 'medication_dose_7',
+        '#medication_dose_8': 'medication_dose_8',
+        '#medication_dose_9': 'medication_dose_9',
+        '#medication_freq_0': 'medication_freq_0',
+        '#medication_freq_1': 'medication_freq_1',
+        '#medication_freq_2': 'medication_freq_2',
+        '#medication_freq_3': 'medication_freq_3',
+        '#medication_freq_4': 'medication_freq_4',
+        '#medication_freq_5': 'medication_freq_5',
+        '#medication_freq_6': 'medication_freq_6',
+        '#medication_freq_7': 'medication_freq_7',
+        '#medication_freq_8': 'medication_freq_8',
+        '#medication_freq_9': 'medication_freq_9',
+        '#dentist_name': 'dentist_name',
+        '#dentist_phone': 'dentist_phone',
+        '#dentist_email': 'dentist_email',
+        '#dental_condition': 'dental_condition',
+
+
+
         '#updated_on': 'updated_on'
     }
 
     attr_values = {
-        ':active': data['active'],
         ':email': data['email'],
         ':password': data['password'],
-        ':user_role': data['user_role'],
         ':first_name': data['first_name'],
         ':last_name': data['last_name'],
+        ':middle_initial': data['middle_initial'],
+        ':home_phone': data['home_phone'],
+        ':mobile_phone': data['mobile_phone'],
+        ':street_address': data['street_address'],
+        ':street_address_2': data['street_address_2'],
+        ':city': data['city'],
+        ':state': data['state'],
+        ':zipcode': data['zipcode'],
+        ':dob': data['dob'],
+        ':height': data['height'],
+        ':weight': data['weight'],
+        ':heart_rate': data['heart_rate'],
+        ':active': data['active'],
+        ':status': data['status'],
+        ':gender': data['gender'],
+        ':patient_preferences': data['patient_preferences'],
+        ':patient_consents': data['patient_consents'],
+        ':family_history': data['family_history'],
+        ':allergies': data['allergies'],
+        ':referrals': data['referrals'],
+        ':lab_results': data['lab_results'],
+        ':care_plan': data['care_plan'],
+        ':lifestyle_history': data['lifestyle_history'],
+        ':social_history': data['social_history'],
+        ':bmi': data['bmi'],
+        ':current_problems_0': data['current_problems_0'],
+        ':current_problems_1': data['current_problems_1'],
+        ':current_problems_2': data['current_problems_2'],
+        ':current_problems_3': data['current_problems_3'],
+        ':current_problems_4': data['current_problems_4'],
+        ':current_problems_5': data['current_problems_5'],
+        ':current_problems_6': data['current_problems_6'],
+        ':current_problems_7': data['current_problems_7'],
+        ':current_problems_8': data['current_problems_8'],
+        ':current_problems_9': data['current_problems_9'],
+        ':medication_name_0': data['medication_name_0'],
+        ':medication_name_1': data['medication_name_1'],
+        ':medication_name_2': data['medication_name_2'],
+        ':medication_name_3': data['medication_name_3'],
+        ':medication_name_4': data['medication_name_4'],
+        ':medication_name_5': data['medication_name_5'],
+        ':medication_name_6': data['medication_name_6'],
+        ':medication_name_7': data['medication_name_7'],
+        ':medication_name_8': data['medication_name_8'],
+        ':medication_name_9': data['medication_name_9'],
+        ':medication_dose_0': data['medication_dose_0'],
+        ':medication_dose_1': data['medication_dose_1'],
+        ':medication_dose_2': data['medication_dose_2'],
+        ':medication_dose_3': data['medication_dose_3'],
+        ':medication_dose_4': data['medication_dose_4'],
+        ':medication_dose_5': data['medication_dose_5'],
+        ':medication_dose_6': data['medication_dose_6'],
+        ':medication_dose_7': data['medication_dose_7'],
+        ':medication_dose_8': data['medication_dose_8'],
+        ':medication_dose_9': data['medication_dose_9'],
+        ':medication_freq_0': data['medication_freq_0'],
+        ':medication_freq_1': data['medication_freq_1'],
+        ':medication_freq_2': data['medication_freq_2'],
+        ':medication_freq_3': data['medication_freq_3'],
+        ':medication_freq_4': data['medication_freq_4'],
+        ':medication_freq_5': data['medication_freq_5'],
+        ':medication_freq_6': data['medication_freq_6'],
+        ':medication_freq_7': data['medication_freq_7'],
+        ':medication_freq_8': data['medication_freq_8'],
+        ':medication_freq_9': data['medication_freq_9'],
+        ':blood_pressure_systolic': data['blood_pressure_systolic'],
+        ':blood_pressure_diastolic': data['blood_pressure_diastolic'],
         ':updated_on': timestamp
     }
 
@@ -54,44 +197,115 @@ def update(event, context):
         UpdateExpression='SET #password= :password, '
                          '#name.#first= :first_name, '
                          '#name.#last= :last_name, '
+                         '#name.#middle_initial= :middle_initial, '
+                         '#home_address.#street_address= :street_address, '
+                         '#home_address.#street_address_2= :street_address_2, '
+                         '#home_address.#city= :city, '
+                         '#home_address.#state= :state, '
+                         '#home_address.#zipcode= :zipcode, '
+                         '#phone.#home= :home_phone, '
+                         '#phone.#mobile= :mobile_phone, '
                          '#email= :email, '
+                         '#dob= :dob, '
+                         '#height= :height, '
+                         '#weight= :weight, '
+                         '#heart_rate= :heart_rate, '
                          '#active= :active, '
-                         '#updated_on= :updated_on, '
-                         '#user_role= :user_role ',
+                         '#status= :status, '
+                         '#gender= :gender, '
+                         '#patient_consents= :patient_consents, '
+                         '#patient_preferences= :patient_preferences, '
+                         '#family_history= :family_history, '
+                         '#allergies= :allergies, '
+                         '#referrals= :referrals, '
+                         '#lab_results= :lab_results, '
+                         '#care_plan= :care_plan, '
+                         '#lifestyle_history= :lifestyle_history, '
+                         '#social_history= :social_history, '
+                         '#bmi= :bmi, '
+                         '#current_problems_0= :current_problems_0, '
+                         '#current_problems_1= :current_problems_1, '
+                         '#current_problems_2= :current_problems_2, '
+                         '#current_problems_3= :current_problems_3, '
+                         '#current_problems_4= :current_problems_4, '
+                         '#current_problems_5= :current_problems_5, '
+                         '#current_problems_6= :current_problems_6, '
+                         '#current_problems_7= :current_problems_7, '
+                         '#current_problems_8= :current_problems_8, '
+                         '#current_problems_9= :current_problems_9, '
+                         '#medication_name_0= :medication_name_0, '
+                         '#medication_name_1= :medication_name_1, '
+                         '#medication_name_2= :medication_name_2, '
+                         '#medication_name_3= :medication_name_3, '
+                         '#medication_name_4= :medication_name_4, '
+                         '#medication_name_5= :medication_name_5, '
+                         '#medication_name_6= :medication_name_6, '
+                         '#medication_name_7= :medication_name_7, '
+                         '#medication_name_8= :medication_name_8, '
+                         '#medication_name_9= :medication_name_9, '
+                         '#medication_dose_0= :medication_dose_0, '
+                         '#medication_dose_1= :medication_dose_1, '
+                         '#medication_dose_2= :medication_dose_2, '
+                         '#medication_dose_3= :medication_dose_3, '
+                         '#medication_dose_4= :medication_dose_4, '
+                         '#medication_dose_5= :medication_dose_5, '
+                         '#medication_dose_6= :medication_dose_6, '
+                         '#medication_dose_7= :medication_dose_7, '
+                         '#medication_dose_8= :medication_dose_8, '
+                         '#medication_dose_9= :medication_dose_9, '
+                         '#medication_freq_0= :medication_freq_0, '
+                         '#medication_freq_1= :medication_freq_1, '
+                         '#medication_freq_2= :medication_freq_2, '
+                         '#medication_freq_3= :medication_freq_3, '
+                         '#medication_freq_4= :medication_freq_4, '
+                         '#medication_freq_5= :medication_freq_5, '
+                         '#medication_freq_6= :medication_freq_6, '
+                         '#medication_freq_7= :medication_freq_7, '
+                         '#medication_freq_8= :medication_freq_8, '
+                         '#medication_freq_9= :medication_freq_9, '
+                         '#blood_pressure_systolic= :blood_pressure_systolic, '
+                         '#blood_pressure_diastolic= :blood_pressure_diastolic, '
+                         '#updated_on= :updated_on ',
         ReturnValues='ALL_NEW',
     )
 
-    # create a response
-    #response = {
-    #    "statusCode": 200,
-    #    "body": json.dumps(result['Attributes'],
-    #                       cls=decimalencoder.DecimalEncoder)
-    #}
-
-
     response = {
         "statusCode": 200,
-        "body": json.dumps(event['pathParameters']['id'])
+        "body": json.dumps({"message":"Success", "id":event['pathParameters']['id']})
     }
-
     return response
 
 if __name__ == '__main__':
     ## Configure boto for local environment
     boto3.setup_default_session(profile_name='serverless')
-    os.environ["DYNAMODB_TABLE"] = 'medifax-backend-employees-dev'
+    os.environ["DYNAMODB_TABLE"] = 'medifax-backend-customers-dev'
 
     response = {
         "pathParameters": {
-            "id": "248f670c-1796-11e8-94ff-aac4be946e9b"
+            "id": "5da5294a-36d7-11e8-8eae-2687762a2f75"
         },
         "body": json.dumps({
-		"first_name": "123BRYAN-BRY",
-		"last_name": "456RICHARD-RIC",
-		"email": "www555@domain.com",
+		"first_name": "Bryan",
+		"last_name": "Richard",
+        "middle_initial": "J",
+		"email": "bryan@furnaceai.com",
 		"password": "",
-		"user_role": "testing",
-		"active": "false"
+        "home_phone": "",
+        "mobile_phone": "8019539821",
+		"street_address": "3356 S 1100 E",
+        "street_address_2": "",
+        "city": "St. George",
+        "state": "UT",
+        "zipcode": "84106",
+        "dob": "10/17/1972",
+        "gender": 'Male',
+        "blood_pressure_systolic": '120',
+        "blood_pressure_diastolic": '80',
+        "height": '70',
+        "heart_rate": '64',
+        "active": 'Yes',
+        "status": 'Awaiting HIPAA Consent',
+        "weight": '164'
 	})
     }
     # testing_data = json.dumps({'body':})
