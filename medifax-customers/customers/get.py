a4cd8498e2b3281cb513a40decdbb6e56affdc89
Bryan Richard <bryan@furnaceai.com>
2018-04-21 19:29:58 -0600

    Fetch images attached to the customer account from S3

--- medifax-customers/customers/get.py
+++ medifax-customers/customers/get.py
@@ -3,6 +3,8 @@ import json
 from libs import decimalencoder
 import boto3
 from boto3.dynamodb.conditions import Key
+from boto.s3.connection import S3Connection
+
 
 def get(event, context):
     dynamodb = boto3.resource('dynamodb')
@@ -12,12 +14,29 @@ def get(event, context):
             'id': event['pathParameters']['id']
         }
     )
+
+    # Add a key for the images
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(name="medifax-images")
+    result['Item']['images'] = {}
+    IMG_TYPES = ['medical', 'dental']
+
+    # Fetch the two types of images and append them to the JSON response
+    for img in IMG_TYPES:
+        result['Item']['images'][img] = []
+        # Fetch Medical Images
+        prefix = "%s/%s/" % (event['pathParameters']['id'], img)
+        FilesNotFound = True
+        for obj in bucket.objects.filter(Prefix=prefix):
+            result['Item']['images'][img].append('https://s3.amazonaws.com/{0}/{1}'.format(bucket.name, obj.key))
+            FilesNotFound = False
+        if FilesNotFound:
+            pass
     response = {
         "statusCode": 200,
         "body": json.dumps(result['Item'],
                            cls=decimalencoder.DecimalEncoder)
     }
-
     return response
 
 if __name__ == '__main__':
@@ -25,7 +44,7 @@ if __name__ == '__main__':
     os.environ["DYNAMODB_TABLE"] = 'medifax-backend-customers-dev'
     payload = {
         "pathParameters": {
-            "id": "dba29cee-23d4-11e8-bd8a-acbc3294be4b"
+            "id": "c85cf638-3aa0-11e8-8bb4-5e566f9ab6c7"
         }
     }
     data = json.loads(json.dumps(payload))
